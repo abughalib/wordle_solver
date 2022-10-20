3b9c782cd180fd9282815466b0da3e99bca1bc1b
Abu <abughalib64@gmail.com>
2022-10-20 22:58:16 +0530

    added cors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{middleware::Logger, web, App, HttpServer};
 
 pub mod api;
 pub mod database;
@@ -11,10 +12,19 @@ extern crate diesel;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
     HttpServer::new(|| {
         App::new()
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allow_any_header()
+                    .allowed_methods(vec!["GET", "POST"])
+                    .max_age(3600),
+            )
+            .wrap(Logger::default())
             .service(api::index)
-            .route("word", web::post().to(api::word))
+            .route("words", web::post().to(api::word))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
