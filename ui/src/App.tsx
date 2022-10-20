a48d3db1e81c41a20d4c4c43398d8c97c82ccc71
Abu <abughalib64@gmail.com>
2022-10-20 22:59:35 +0530

    word api fetching

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,41 @@
 import "./App.css";
+import React, { useState } from "react";
+
+async function fetchWords(): Promise<[]> {
+  let resp = await fetch("http://localhost:8080/words", {
+    method: "POST",
+    body: JSON.stringify({
+      length: 19,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (resp.status === 200) {
+    let json_resp = await resp.json();
+    return json_resp;
+  } else {
+    return await Promise.reject(resp);
+  }
+}
 
 function App() {
+  const [words, setWords] = useState<[]>([]);
+
+  React.useEffect(() => {
+    fetchWords().then((words) => {
+      if (words) {
+        setWords(words);
+      }
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello There!</h1>
+        {words.map((word) => {
+          return <p key={word}>{word}</p>;
+        })}
       </header>
     </div>
   );
